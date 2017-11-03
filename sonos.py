32b6e54d4c2cad4b814be2dcaeab7675e0256c59
mikepartelow <github@mikepartelow.com>
2017-11-02 21:02:32 -0700

    better

--- sonos.py
+++ sonos.py
@@ -11,12 +11,6 @@ def get_coordinator():
     # print(dir(list(zps)[0]))
     return next(zp for zp in zps if zp.is_coordinator)
 
-def make_playlists_dir():
-    try:
-        os.makedirs('./playlists')
-    except OSError:
-        pass
-
 def enqueue_playlist(zp, playlist_path):
     with open(playlist_path, "rb") as f:
         playlist = json.loads(f.read())
@@ -34,20 +28,20 @@ def enqueue_playlist(zp, playlist_path):
 
         zp.add_to_queue(item)
 
-def dump_playlists(zp):
+def dump_playlists(zp, playlists_dir):
     for playlist in zp.get_sonos_playlists():
         print(playlist.title)
 
         tracks = [ track.to_dict() for track in zp.music_library.browse(playlist) ]
 
-        with open("playlists/{}.json".format(playlist.title), "wb") as f:
+        with open("{}/{}.json".format(playlists_dir, playlist.title), "wb") as f:
             f.write(json.dumps(tracks))
 
-def dump_queue(zp):
+def dump_queue(zp, playlists_dir):
     timestamp = datetime.datetime.today().strftime("%Y%m%d%H%M%S")
     tracks = [ item.to_dict() for item in zp.get_queue() ]
 
-    with open("playlists/queue.{}.json".format(timestamp), "wb") as f:
+    with open("{}/queue.{}.json".format(playlists_dir, timestamp), "wb") as f:
         f.write(json.dumps(tracks))
 
 if __name__ == "__main__":
@@ -56,23 +50,19 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='SONOS queue and playlist manipulation.')
     parser.add_argument('command', action='store', type=str, choices=['dump-playlists', 'dump-queue', 'enqueue'],
                         help='what to do')
-    parser.add_argument('--path', action='store', type=str, default=None,
-                        help='path to playlist to enqueue')
+    parser.add_argument('path', action='store', type=str,
+                        help='path to playlist to enqueue, or path to directory for playlist/queue dump')
 
     args = parser.parse_args()
 
-    if args.command == 'dump-playlists':
-        make_playlists_dir()
-        dump_playlists(get_coordinator())
-    elif args.command == 'dump-queue':
-        make_playlists_dir()
-        dump_queue(get_coordinator())
-    elif args.command == 'enqueue':
-        if args.path is None:
-            parser.exit(1, "ERROR: enqueue command requires --path\n")
-        elif not os.path.exists(args.path):
-            parser.exit(2, "ERROR: path {} not found\n".format(args.path))
+    if not os.path.exists(args.path):
+        parser.exit(2, "ERROR: path {} not found\n".format(args.path))
 
+    if args.command == 'dump-playlists':
+        dump_playlists(get_coordinator(), args.path)
+    elif args.command == 'dump-queue':
+        dump_queue(get_coordinator(), args.path)
+    elif args.command == 'enqueue':
         enqueue_playlist(get_coordinator(), args.path)
 
     parser.exit(0)
